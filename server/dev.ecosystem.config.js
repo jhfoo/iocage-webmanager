9d62e9012e7f961aa506f1df39e1aa67864d5737
Foo JH <foojihaw@hotmail.com>
2019-01-22 21:38:56 -0600

    basic jail start/ stop apis

---
+++ server/dev.ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    apps: [{
+        name: "iocage-webmanager-dev",
+        script: "src/server.js",
+        watch: ['src']
+    }]
+};
--- server/src/router-iocage.js
+++ server/src/router-iocage.js
@@ -1,4 +1,5 @@
-const assert = require('assert'),
+const child_process = require('child_process'),
+    assert = require('assert'),
     Router = require('restify-router').Router,
     router = new Router(),
     log4js = require('log4js'),
@@ -9,5 +10,44 @@ router.get('/ping', (req, res, next) => {
     res.send('pong');
     next();
 });
-    
+
+router.get('/list', (req, res, next) => {
+    child_process.exec('iocage list', (err, stdout, stderr) => {
+        if (err) {
+            res.send(err);
+            next();
+        } else {
+            logger.debug(stdout);
+            res.send(stdout);
+            next();
+        }
+    });
+});
+
+router.get('/start/:jailname', (req, res, next) => {
+    child_process.exec('iocage start ' + req.params.jailname, (err, stdout, stderr) => {
+        if (err) {
+            res.send(err);
+            next();
+        } else {
+            logger.debug(stdout);
+            res.send(stdout);
+            next();
+        }
+    });
+});
+
+router.get('/stop/:jailname', (req, res, next) => {
+    child_process.exec('iocage stop ' + req.params.jailname, (err, stdout, stderr) => {
+        if (err) {
+            res.send(err);
+            next();
+        } else {
+            logger.debug(stdout);
+            res.send(stdout);
+            next();
+        }
+    });
+});
+
 module.exports = router;
